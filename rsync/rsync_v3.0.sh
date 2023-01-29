d57a7d7d2cf1ba1bcd04703d07da2fcf4105008f
iamshf <haofengsheng@gmail.com>
2023-01-29 11:13:22 +0800

    replace ip

--- rsync/rsync_v3.0.sh
+++ rsync/rsync_v3.0.sh
@@ -15,7 +15,7 @@ echo '' >> $LOG_FILE;
 /usr/bin/rsync -avzP --delete -e '/usr/bin/ssh -p 8704' "user@sourceIP:/data2/web/" "/data/web/"
 
 
-find /data/web/ -name "Apis.php" -print0 | xargs -0 perl -pi -e 's|http://192.168.0.61/|http://123.57.7.113/|g'
+find /data/web/ -name "Apis.php" -print0 | xargs -0 perl -pi -e 's|http://sourceHostIP/|http://targetHostIP/|g'
 
 #修改正式版Memcached
 find /data/web/ -name "Conf.inc" -print0 | xargs -0 perl -pi -e "s|array\('sourceMemcachedIP',\s*11211\)|array('targetMemcachedIP', 11211)|g"
@@ -36,7 +36,7 @@ find /data/web/ -name "Conf.inc" -print0 | xargs -0 perl -pi -e "s|/data2/web/|/
 
 /usr/bin/rsync -avzP --delete -e '/usr/bin/ssh -p 8704' "user@sourceIP:/etc/nginx/conf.d/" "/etc/nginx/conf.d/" --exclude="BsccallOfficial.conf"
 find /etc/nginx/conf.d/ -name "*.conf" -print0 | xargs -0 perl -pi -e "s|/data2/web/|/data/web/|g"
-find /etc/nginx/conf.d/ -name "*.conf" -print0 | xargs -0 perl -pi -e "s|http://192.168.0.96|http://202.85.217.96|g"
+find /etc/nginx/conf.d/ -name "*.conf" -print0 | xargs -0 perl -pi -e "s|http://sourceProxyIP|http://targetProxyIP|g"
 find /etc/nginx/conf.d/ -name "*.conf" -print0 | xargs -0 perl -pi -e "s|\s*fastcgi_pass 127.0.0.1:9000;|        include fastcgi.conf;\n	    fastcgi_pass unix:/run/php-fpm/www.sock;|g"
 
 /usr/sbin/nginx -s reload
