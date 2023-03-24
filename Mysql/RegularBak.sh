9dc64c2cede76c85676aba1453c1257d85610035
iamshf <haofengsheng@gmail.com>
2023-03-24 10:09:34 +0800

    format

--- Mysql/RegularBak.sh
+++ Mysql/RegularBak.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+
 file_suffix=$(date +%Y%m%d%H%M)
-del_folder="/mydata/mysqlbak/$(date -d -60-day +%Y%m%d)"
-new_folder="/mydata/mysqlbak/$(date +%Y%m%d)"
+del_folder="/data/mysqlbak/$(date -d -60-day +%Y%m%d)"
+new_folder="/data/mysqlbak/$(date +%Y%m%d)"
+
+SOURCE_USER=user
+SOURCE_PWD='pwd'
+SOURCE_HOST='host'
+SOURCE_PORT=3306
+
+SQL="SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('mysql','information_schema','performance_schema', 'log')"
+
+tmpFile="/data/mysqlbak/databases"
+
+/usr/bin/mysql -u${SOURCE_USER} -p${SOURCE_PWD} -h${SOURCE_HOST} -P${SOURCE_PORT} -ANe "$SQL" > $tmpFile
 
 if [ ! -d $new_folder ]; then
     /usr/bin/mkdir $new_folder
 fi
 
-for db in "db1" "db2" "db……"
+
+for DB in `cat ${tmpFile}`;
 do
-    /usr/bin/mysqldump -uBothshf -ppassword -B $db | /usr/bin/gzip -9 > ${new_folder}/${db}_${file_suffix}.sql.gz
+    /usr/bin/mysqldump -u${SOURCE_USER} -p${SOURCE_PWD} -h${SOURCE_HOST} -P${SOURCE_PORT} --routines --hex-blob -B ${DB}  | /usr/bin/gzip -9 > ${new_folder}/${DB}_${file_suffix}.sql.gz;
 done
 
 /usr/bin/rm -rf $del_folder
+/usr/bin/rm $tmpFile;
 
-/usr/bin/rsync -a --rsh="/usr/bin/ssh -p8704" --delete --ignore-errors --force /mydata/mysqlbak/ root@192.168.0.61:/data/mysqlDatabases/BAK/
+/usr/bin/rsync -a --rsh="/usr/bin/ssh -p8704" --delete --ignore-errors --force /data/mysqlbak/ Bothshf@192.168.0.61:/data/ftp/shfftp/mysqlDatabases/BAK/ --exclude="bak.sh";
